07f013406cc84a3f34844bbc87b826a272e804ab
bprado <bastian.prado.v@gmail.com>
2024-08-15 20:26:40 -0700

    Add styling to payment

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -48,10 +48,12 @@ const Cart = () => {
                             <p>Subtotal</p>
                             <p>${getTotalCartAmount()}</p>
                         </div>
+                        <hr/>
                         <div className="cart-total-details">
                             <p>Delivery Fee</p>
                             <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
                         </div>
+                        <hr/>
                         <div className="cart-total-details">
                             <b>Total</b>
                             <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
